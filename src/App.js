19e5f069d533b946f9511169a25c956679bf067d
sakshi16-b <sakshibisht016@gmail.com>
2025-05-16 08:39:37 +0000

    done

--- src/App.js
+++ src/App.js
@@ -3,40 +3,46 @@ import "./App.css";
 import User from "./User";
 
 function App() {
-  const userData = [
-    {
-      name: "Anil",
-      age: "29",
-      email: "anil@test.com",
-      id: 1,
-    },
-    {
-      name: "sam",
-      age: "34",
-      email: "sam@test.com",
-      id: 2,
-    },
-    {
-      name: "peter",
-      age: "20",
-      email: "peter@test.com",
-      id: 3,
-    },
-    {
-      name: "bruce",
-      age: "50",
-      email: "bruce@test.com",
-      id: 4,
-    },
-  ];
+  const name = "peter";
+  const userObj = {
+    name: "anil sidhu",
+    email: "anil@test.com",
+    age: 29,
+  };
+  const userArray = ["sam", "peter", "bruce"];
+  let x = 10;
+  let y = 20;
+  let path =
+    "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/3/22/0/shutterstock_national-puppy-day-224423782.jpg.rend.hgtvcom.616.462.suffix/1521744674350.jpeg";
+  function fruit() {
+    return "Apple";
+  }
+  function sum(a, b) {
+    return a + b;
+  }
+  function operation(a, b, op) {
+    if (op == "+") {
+      return a + b;
+    } else if (op == "-") {
+      return a - b;
+    } else {
+      return a * b;
+    }
+  }
+
   return (
-    <div>
-      <h1>Loop in JSX with map function</h1>
-      {userData.map((user) => (
-        <div key={user.id}>
-          <User data={user} />
-        </div>
-      ))}
+    <div className="App">
+      <h1>JSX with Curly Braces</h1>
+      <h1>{name ? name : "user not found"}</h1>
+      <h1>{x + y}</h1>
+      <h1> {fruit()}</h1>
+      <h1>{sum(10, 100)}</h1>
+      <h1>{operation(20, 10, "")}</h1>
+      <h1>{userObj.age}</h1>
+      <h1>{userArray[0]}</h1>
+      <input type="text" value={name} id={name} />
+      <br />
+      <img src={path} />
     </div>
   );
 }
